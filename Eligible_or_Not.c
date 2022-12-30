11b5d70bbae38089df486788131826c4ae31f3e8
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2022-12-30 18:33:45 +0530

    #include<stdio.h> int main() {     int t,x,y,a;     scanf("%d",&t);     while(t)     {         scanf("%d%d%d",&x,&y,&a);         if(a>=x&&a<y)         {             printf("YES ");         }         else         {             printf("NO ");         }         t--;     } }

---
+++ Eligible_or_Not.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int t,x,y,a;
+    scanf("%d",&t);
+    while(t)
+    {
+        scanf("%d%d%d",&x,&y,&a);
+        if(a>=x&&a<y)
+        {
+            printf("YES
+");
+        }
+        else
+        {
+            printf("NO
+");
+        }
+        t--;
+    }
+}
