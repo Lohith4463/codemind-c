0e17366e21c9f0b9df8a01c7950234cde84a0d64
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-09-10 10:03:32 +0530

    #include<stdio.h> int perf(int num) {     int i;     for(i=1;i*i<=num;i++){         if(num%i==0 && num/i==i){             return 1;         }     }     return 0; } int main(){     int n;     scanf("%d",&n);     if(perf(n)){         printf("True");     }     else{         printf("False");     } }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
-#include<math.h>
-int main()
+int perf(int num)
 {
-    int n;
     int i;
-    float f;
+    for(i=1;i*i<=num;i++){
+        if(num%i==0 && num/i==i){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main(){
+    int n;
     scanf("%d",&n);
-    f=sqrt((double)n);
-    i=f;
-    if(i==f)
-    printf("True",n);
-    else 
-    printf("False",n);
+    if(perf(n)){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
 }
